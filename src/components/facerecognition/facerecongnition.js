ba8a76d36d58324a42d6de6915c02767d190a7b2
prince kumar <princekumarcena@gmail.com>
2019-09-18 21:57:31 +0530

    resolve

--- src/components/facerecognition/facerecongnition.js
+++ src/components/facerecognition/facerecongnition.js
@@ -5,8 +5,8 @@ const Facerecognition = ({box, imageUrl }) => {
     return(
         <div className="container" >
 
-        <div  className="absolute imgs mt2 ">
-         <img id="inputimage" className=" mt2"  src={imageUrl} width="400px"  alt='' height="400px" />
+        <div  className="absolute mt2 ">
+         <img id="inputimage" className=" mt2"  src={imageUrl} width="400px"  alt='' height="auto" />
           <div className="bounding-box" style={{top: box.topRow,right: box.rightCol, left:box.leftCol,bottom: box.bottomRow }}></div>
 
           </div>
