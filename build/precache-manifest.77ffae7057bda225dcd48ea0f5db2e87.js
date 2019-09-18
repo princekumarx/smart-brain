2955d33787c6b573c1f0ced1aba37a3b84ff1e0c
prince kumar <princekumarcena@gmail.com>
2019-09-18 10:34:13 +0530

    build

---
+++ build/precache-manifest.77ffae7057bda225dcd48ea0f5db2e87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d8fbe74ded90e882b4a517e8ad4e499",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1bf8f1a409ed9ed8728",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "81a6529eac3b9b440392",
+    "url": "/static/css/main.7b616ae7.chunk.css"
+  },
+  {
+    "revision": "f1bf8f1a409ed9ed8728",
+    "url": "/static/js/2.38a5ea96.chunk.js"
+  },
+  {
+    "revision": "81a6529eac3b9b440392",
+    "url": "/static/js/main.8a0f17be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
